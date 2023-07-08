0f401e19108546fe685df536be33afd199784926
ankit yadav <ankit6686482@gmail.com>
2023-07-08 14:15:33 +0530

    Enabled Search Feature

--- Frontend/src/Components/searchResults.jsx
+++ Frontend/src/Components/searchResults.jsx
@@ -1,21 +1,60 @@
 import { MusicCards } from "./recommendation";
 import '../styles/musicRecommendation.css';
 import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SearchResults = () =>{
     const [searchParams, setSearchParams] = useSearchParams();
+    const [searchedSongs, setSearchedSongs] = useState(null);
+    const [searchedAlbums, setSearchedAlbums] = useState(null);
+    const [searchedArtists, setSearchedArtists] = useState(null);
+    const [searchedPlaylists, setSearchedPlaylists] = useState(null);
+
     var searchHeading
-    if(searchParams.get('q')==null || searchParams.get('q')=='')
+    var searchQuery = searchParams.get('q');
+    if(searchQuery===null || searchQuery==='')
         searchHeading = "Search Results will appear below";
-    else
-        searchHeading = "Search Results for "+searchParams.get('q');
+    else{
+        searchHeading = "Search Results for "+searchQuery;
+    }
+
+    useEffect(() => {
+        if(searchQuery!=null){
+            const baseUrl = 'http://localhost:5000/search?q='+searchQuery+'&type=';
+            let type = "songs";
+            axios.get(baseUrl+type)
+            .then((songs) => {
+                setSearchedSongs(songs.data);
+            })
+
+            type = "albums";
+            axios.get(baseUrl+type)
+            .then((albums) => {
+                setSearchedAlbums(albums.data);
+            })
+
+            type = "artists";
+            axios.get(baseUrl+type)
+            .then((artists) => {
+                setSearchedArtists(artists.data);
+            })
+
+            type = "playlists";
+            axios.get(baseUrl+type)
+            .then((playlists) => {
+                setSearchedPlaylists(playlists.data);
+            })
+        }
+    }, [searchQuery]);
+
     return(
         <div className="music-recommendation">
             <h2>{searchHeading}</h2>
-            <MusicCards title = "Songs"></MusicCards>
-            <MusicCards title = "Albums"></MusicCards>
-            <MusicCards title = "Artists"></MusicCards>
-            <MusicCards title = "Playlists"></MusicCards>
+            {searchedSongs!=null && <MusicCards title = "Songs" dataSet={searchedSongs} />}
+            {searchedAlbums!=null && <MusicCards title = "Albums" dataSet={searchedAlbums} />}
+            {searchedArtists!=null && <MusicCards title = "Artists" dataSet={searchedArtists} type="artists" />}
+            {searchedPlaylists!=null && <MusicCards title = "Playlists" dataSet={searchedPlaylists} type="playlists" />}
         </div>
     )
 }
--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
-app.get('/api',(req,res) => {
-    res.json({'users':['ankit', 'yadav']})
+app.use(cors());
+
+app.get('/search', async (req, res) => {
+    const YTMusic = require("ytmusic-api").default;
+    const ytmusic = await new YTMusic().initialize();
+    const q = req.query['q'];
+    const type = req.query['type'];
+    if(type == "songs")
+        res.send(await ytmusic.searchSongs(q));
+    else if(type == "albums")
+        res.send(await ytmusic.searchAlbums(q));
+    else if(type == "artists")
+        res.send(await ytmusic.searchArtists(q));
+    else if(type == "playlists")
+        res.send(await ytmusic.searchPlaylists(q));
+    else
+        res.status(404).send("Invalid type argument");
 })
 
 app.listen(5000, () => {console.log('Running on port 5000')})
