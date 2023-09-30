e868af21f6067684eb2b0d320ec185571635de9f
ankit yadav <ankit6686482@gmail.com>
2023-09-30 20:35:59 +0530

    added backend signup

--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, {useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { login } from "../state/action-creators";
+import { useDispatch } from "react-redux";
 
-const Signup = () => {
+const  SERVER = process.env.REACT_APP_AUTH_SERVER;
+
+const Signup = (props) => {
+  const dispatch = useDispatch();
+
+  const [credential, setCredential] = useState({ name:"", email:"",password:"",cpassword:""})
+    let navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(credential)
+    const {name,email,password}=credential
+    const response = await fetch(`${SERVER}api/auth/createuser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,email,password
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // Save the auth token and redirect
+      localStorage.setItem("token", json.authtoken);
+      dispatch(login(json.authtoken))
+      navigate("/");
+       console.log('success')
+    } else {
+      // props.showAlert("Invalid Details","danger");
+      console.log('failse')
+    }
+  };
+
+  const onChange = (e) => {
+    setCredential({ ...credential, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="form-box">
       <div className="header-form">
@@ -10,7 +50,7 @@ const Signup = () => {
         </h4>
       </div>
       <div className="body-form">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span className="input-group-text">
@@ -20,8 +60,10 @@ const Signup = () => {
             <div></div>
             <input
               type="text"
+              name="name"
               className="form-control"
               placeholder="Full Name"
+              onChange={onChange}
             />
           </div>
           <div className="input-group mb-3">
@@ -33,8 +75,10 @@ const Signup = () => {
             <div></div>
             <input
               type="text"
+              name="email"
               className="form-control"
               placeholder="Email"
+              onChange={onChange}
             />
           </div>
           <div className="input-group mb-3">
@@ -45,8 +89,10 @@ const Signup = () => {
             </div>
             <input
               type="password"
+              name="password"
               className="form-control"
               placeholder="Password"
+              onChange={onChange}
             />
           </div>
           <div className="input-group mb-3">
@@ -57,11 +103,13 @@ const Signup = () => {
             </div>
             <input
               type="password"
+              name="cpassword"
               className="form-control"
               placeholder="Confirm Password"
+              onChange={onChange}
             />
           </div>
-          <button type="button" className="btn btn-secondary btn-block">
+          <button type="submit" className="btn btn-secondary btn-block">
             SIGNUP
           </button>
           <div className="message">
