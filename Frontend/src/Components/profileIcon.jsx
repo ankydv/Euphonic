94e6598b21a7e20ff8a9d03afd49c6541f8b1084
ankit yadav <ankit6686482@gmail.com>
2023-10-05 22:38:38 +0530

    added profile letter

--- Frontend/src/Components/profileIcon.jsx
+++ Frontend/src/Components/profileIcon.jsx
@@ -4,11 +4,32 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { logout } from "../state/action-creators";
 import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const  SERVER = process.env.REACT_APP_AUTH_SERVER;
 
 const ProfileIcon = () => {
 
+  const [currUser, setCurrUser] = useState(null);
+
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const headers = {
+      'auth-token': localStorage.getItem('token')
+    };
+  
+    axios.post(`${SERVER}api/auth/getuser`, {}, { headers })
+      .then(res => {
+        setCurrUser(res.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [isLoggedIn]);
+  
 
   const handleProfile = () =>{
     window.open('https://inotebook-496bd.web.app/signup', '_blank');
@@ -24,8 +45,7 @@ const ProfileIcon = () => {
     {isLoggedIn?
     <>
     <div className="profileIcon" onClick={handleProfile}>
-      <div className="headIcon"></div>
-      <div className="bodyIcon" />
+      {currUser && <h3>{currUser['name'].charAt(0).toUpperCase()}</h3>}
     </div>
     <button onClick={handleLogout}>Logout</button>
     </>
