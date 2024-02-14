9335a8c5f7e3ad804d8b214531a425ee83087b15
ankit yadav <ankit6686482@gmail.com>
2024-02-14 19:07:21 +0530

    fix useless import

--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -14,7 +14,6 @@ import { useDispatch } from "react-redux";
 import { login } from "../state/action-creators";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import OtpModal from "./OtpModal";
 
 const SERVER = process.env.REACT_APP_AUTH_SERVER;
 
